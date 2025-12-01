f431465a3401aa33cf7a028d1ea126eba9e96e98
gotunfatra <fatrabawana@gmail.com>
2025-12-02 00:55:14 +0700

    Fix URL API

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    // KONFIGURASI PROXY (PENTING AGAR LOCALHOST JALAN)
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Arahkan ke Backend Node.js
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
